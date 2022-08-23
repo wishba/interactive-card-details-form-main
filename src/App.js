e51b2eb59b77f66be7b74bae60a732087ac9cebc
wishba <wisnubayu34@gmail.com>
2022-08-23 18:08:15 +0700

    base

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <section>
+          <p>0000 0000 0000 0000</p>
+          <p>Jane Appleseed</p>
+          <p>00/00</p>
+        </section>
+
+        <section>
+          <p>000</p>
+        </section>
+
+        <section>
+          <form action="complete.html" method="get">
+            <label>Cardholder Name</label>
+            <input type="text" placeholder="e.g. Jane Appleseed" />
+            <label>Card Number</label>
+            <input type="text" placeholder="e.g. 1234 5678 9123 0000" />
+            <label>Exp. Date (MM/YY)</label>
+            <input type="text" placeholder="MM" />
+            <input type="text" placeholder="YY" />
+            <label>CVC</label>
+            <input type="text" placeholder="e.g. 123" />
+            <input type="submit" value="Confirm" />
+          </form>
+        </section>
+      </main>
+      {/* <footer>
+        <div class="attribution">
+          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+          Coded by <a href="#">Your Name Here</a>.
+        </div>
+      </footer> */}
     </div>
   );
 }
