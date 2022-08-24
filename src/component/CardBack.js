42d57d3fec7290ee67ffe58068634794f3c69bf8
wishba <wisnubayu34@gmail.com>
2022-08-24 12:05:04 +0700

    card back position

--- src/component/CardBack.js
+++ src/component/CardBack.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function CardBack(props) {
   return (
-    <div className='card__back'>
-      <p>{props.cardCVC ? props.cardCVC : '000'}</p>
+    <div className='card__back-container'>
+      <p className='card__back'>{props.cardCVC ? props.cardCVC : '000'}</p>
     </div>
   )
 }
