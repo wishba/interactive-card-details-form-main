eb59907f864a04e261f0467aef822668d377203c
wishba <wisnubayu34@gmail.com>
2022-08-24 11:49:14 +0700

    card front text

--- src/component/CardFront.js
+++ src/component/CardFront.js
@@ -2,14 +2,17 @@ import React from 'react'
 
 export default function CardFront(props) {
   return (
-    <div className='card__front'>
-      <p>{props.cardNumber ? props.cardNumber : '0000 0000 0000 0000'}</p>
-      <p>{props.cardName ? props.cardName : 'JANE APPLESEED'}</p>
-      <p>
-        <span>{props.cardM ? props.cardM : '00'}</span>
-        /
-        <span>{props.cardY ? props.cardY : '00'}</span>
-      </p>
+    <div className="card__front-container">
+      <div className='card__front'>
+        <p>{props.cardNumber ? props.cardNumber : '0000 0000 0000 0000'}</p>
+        <p>{props.cardName ? props.cardName : 'JANE APPLESEED'}
+          <span className='card__front-date'>
+            {props.cardM ? props.cardM : '00'}
+            /
+            {props.cardY ? props.cardY : '00'}
+          </span>
+        </p>
+      </div>
     </div>
   )
 }
