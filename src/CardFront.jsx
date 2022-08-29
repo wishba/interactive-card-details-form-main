31e00f5204faee6528009286c0c07408d9d74157
wishba <wisnubayu34@gmail.com>
2022-08-29 09:52:53 +0700

    card text style

--- src/CardFront.jsx
+++ src/CardFront.jsx
@@ -5,7 +5,7 @@ export default function CardFront(props) {
     <div className='card__front'>
       <div className="card__front-contain">
         <p className='card__number'>{props.cardNumber ? props.cardNumber : '0000 0000 0000 0000'}</p>
-        <p>
+        <p className='card__name'>
           {props.cardName ? props.cardName : 'JANE APPLESEED'}
           <span className="card__date">
             {props.cardM ? props.cardM : '00'}
